e34a96749b9638ecd1949b7f832a4a6905b0ff1c
Thomas Foster <thomas.foster@noaa.gov>
2015-08-14 09:58:18 -0400

    added enable for service

--- puppet/modules/htcondor/manifests/service.pp
+++ puppet/modules/htcondor/manifests/service.pp
@@ -3,6 +3,7 @@ class htcondor::service inherits htcondor {
   service { 'HTCondor Service':
     ensure      => running,
     name        => $htcondor_service,
+    enable      => true,
     hasrestart  => true,
     hasstatus   => true,
     require     => Package['Installing HTCondor'],
